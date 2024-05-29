149242af9e47741eb1459d0552ad23c96293db4d
Leonard Chan <wxclx98@gmail.com>
2024-05-29 18:08:30 +0800

    Render image

--- RayTracing/src/PecanApp.cpp
+++ RayTracing/src/PecanApp.cpp
@@ -2,24 +2,67 @@
 #include "Pecan/EntryPoint.h"
 
 #include "Pecan/Image.h"
+#include "Pecan/Random.h"
+#include "Pecan/Timer.h"
+
+using namespace Pecan;
 
 class ExampleLayer : public Pecan::Layer
 {
 public:
     virtual void OnUIRender() override
     {
-        ImGui::Begin("Hello");
-        ImGui::Button("Button");
+        ImGui::Begin("Settings");
+        ImGui::Text("Last render: %.3fms", m_LastRenderTime);
+        if (ImGui::Button("Render")) {
+            Render();
+        }
         ImGui::End();
 
-        ImGui::ShowDemoWindow();
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("Viewport");
+
+        m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+        m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+        if (m_Image)
+            ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+
+        ImGui::End();
+        ImGui::PopStyleVar();
     }
+
+    void Render()
+    {
+        Timer timer;
+
+        if (!m_Image || m_Image->GetWidth() != m_ViewportWidth || m_Image->GetHeight() != m_ViewportHeight) {
+            m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+            delete[] m_ImageData;
+            m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+        }
+
+        for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
+            m_ImageData[i] = Random::UInt();
+            m_ImageData[i] |= 0xff000000;
+        }
+
+        m_Image->SetData(m_ImageData);
+
+        m_LastRenderTime = timer.ElapsedMillis();
+    }
+
+private:
+    std::shared_ptr<Image> m_Image;
+    uint32_t* m_ImageData = nullptr;
+    uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+    float m_LastRenderTime = 0.0f;
 };
 
 Pecan::Application* Pecan::CreateApplication(int argc, char** argv)
 {
     Pecan::ApplicationSpecification spec;
-    spec.Name = "Pecan Example";
+    spec.Name = "Ray Tracing";
 
     Pecan::Application* app = new Pecan::Application(spec);
     app->PushLayer<ExampleLayer>();
