39e5a7247517e39d80c3cdee5031df0d3e54b484
Leonard Chan <wxclx98@gmail.com>
2024-05-30 15:47:41 +0800

    Ray tracing a circle

---
+++ RayTracing/src/Renderer.h
@@ -0,0 +1,31 @@
+//
+// Created by DasAuto on 5/30/24.
+//
+
+#ifndef RAYTRACING_RENDERER_H
+#define RAYTRACING_RENDERER_H
+
+#include "Pecan/Image.h"
+
+#include "glm/glm.hpp"
+
+class Renderer {
+public:
+    Renderer() = default;
+
+    void Render();
+
+    void OnResize(uint32_t width, uint32_t height);
+
+    std::shared_ptr<Pecan::Image> GetFinalImage() const { return m_FinalImage; };
+
+private:
+    uint32_t PerPixel(glm::vec2 coord);
+
+private:
+    std::shared_ptr<Pecan::Image> m_FinalImage;
+    uint32_t* m_ImageData = nullptr;
+};
+
+
+#endif //RAYTRACING_RENDERER_H
